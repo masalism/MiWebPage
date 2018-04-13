0219c8293f0006b173cd1faa196aff971bf5f752
masalism <mantasmasalis@gmail.com>
2018-04-13 16:07:28 +0300

    paruosta darbui

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("about-mi__mySlides");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1} 
     if (n < 1) {slideIndex = slides.length}
@@ -24,3 +24,19 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
+
+
+//uz funkcijos ribu prisiskyrem kintamaji
+var menu = 0;
+
+function nav() {
+  var x = document.getElementById("myNav");
+  if (menu == 0) {
+    menu = 100;
+  } else {
+    menu = 0;
+  }
+  x.style.width = menu + "%";
+  console.log(menu);
+}
+
